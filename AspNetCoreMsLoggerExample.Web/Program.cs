383855fcde1b2fac31563393f2dfc2b5abbecc53
Jernej Kavka (JK) <jk@ssw.com.au>
2022-02-27 15:05:41 +1000

    ✨ Upgraded MS Logger project.

--- AspNetCoreMsLoggerExample.Web/Program.cs
+++ AspNetCoreMsLoggerExample.Web/Program.cs
@@ -1,58 +1,51 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+// Logging based on https://github.com/jernejk/AspNetCoreSerilogExample
+// NOTE: When upgrading from .NET 5 or earlier, add `<ImplicitUsings>enable</ImplicitUsings>` to **.csproj** file under `<PropertyGroup>`.
+// NOTE: You can still use full Program.cs and Startup.cs if you want to and it's similar to .NET 5.
 
-namespace AspNetCoreMsLoggerExample.Web
+try
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            try
-            {
-                using IHost host = CreateHostBuilder(args).Build();
-                host.Run();
-            }
-            catch (Exception ex)
-            {
-                // Loading configuration or running the application failed.
-                // This will create a logger that can be captured by Azure logger.
-                // To enable Azure logger, in Azure Portal:
-                // 1. Go to WebApp
-                // 2. App Service logs
-                // 3. Enable "Application Logging (Filesystem)", "Application Logging (Filesystem)" and "Detailed error messages"
-                // 4. Set Retention Period (Days) to 10 or similar value
-                // 5. Save settings
-                // 6. Under Overview, restart web app
-                // 7. Go to Log Stream and observe the logs
-                Console.WriteLine("Host terminated unexpectedly: " + ex);
-            }
-        }
+    var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-            => Host.CreateDefaultBuilder(args)
-                   .ConfigureWebHostDefaults(webBuilder =>
-                   {
-                       webBuilder.UseStartup<Startup>()
-                        .CaptureStartupErrors(true)
-                        .ConfigureAppConfiguration(config =>
-                        {
-                            config
-                                // Used for local settings like connection strings.
-                                .AddJsonFile("appsettings.Local.json", optional: true);
-                        })
-                        .ConfigureLogging((hostingContext, loggingBuilder) => {
+    // Register required services.
+    builder.Services.AddControllers();
+    builder.Services.AddLogging(loggingBuilder =>
+    {
+        // NOTE: You can add more log providers here.
 #if DEBUG
-                            // Add Seq for local dev.
-                            loggingBuilder.AddSeq();
+        // Add Seq for local dev.
+        loggingBuilder.AddSeq();
 #endif
-                        });
-                   });
+    });
+
+    WebApplication app = builder.Build();
+
+    // Configuration.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
     }
+
+    // NOTE: If you need UseAuthentication/UseAuthorization, use it after `UseRouting` and before `UseEndpoints`.
+    app.UseRouting();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
+        endpoints.MapGet("", context => context.Response.WriteAsync("Hello World!\nUse /api/test/flatlog?input=Test, /api/test/StructuredLog?input=Test, etc. and observe console/Seq for logs."));
+    });
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    // Loading configuration or running the application failed.
+    // This will create a logger that can be captured by Azure logger.
+    // To enable Azure logger, in Azure Portal:
+    // 1. Go to WebApp
+    // 2. App Service logs
+    // 3. Enable "Application Logging (Filesystem)", "Application Logging (Filesystem)" and "Detailed error messages"
+    // 4. Set Retention Period (Days) to 10 or similar value
+    // 5. Save settings
+    // 6. Under Overview, restart web app
+    // 7. Go to Log Stream and observe the logs
+    Console.WriteLine("Host terminated unexpectedly: " + ex);
 }
--- AspNetCoreSerilogExample.Web/Controllers/TestController.cs
+++ AspNetCoreSerilogExample.Web/Controllers/TestController.cs
@@ -1,66 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 
-namespace AspNetCoreSerilogExample.Web.Controllers
+namespace AspNetCoreSerilogExample.Web.Controllers;
+
+// NOTE: TestController is the same for MS Logger as well as Serilog by design.
+// You can use exactly the same logging for both and only difference is how you configure the 2 loggers.
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class TestController : ControllerBase
 {
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class TestController : ControllerBase
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
     {
-        private readonly ILogger<TestController> _logger;
+        _logger = logger;
+    }
 
-        public TestController(ILogger<TestController> logger)
-        {
-            _logger = logger;
-        }
+    [HttpGet]
+    public void FlatLog(string input)
+    {
+        // NOTE: Don't use flat logs! Use structured logs. (example below)
+        _logger.LogInformation($"Input text: {input}");
+    }
 
-        [HttpGet]
-        public void FlatLog(string input)
-        {
-            _logger.LogInformation($"Input text: {input}");
-        }
+    [HttpGet]
+    public void StructuredLog(string input)
+    {
+        _logger.LogInformation("Input text: {Input}", input);
+    }
 
-        [HttpGet]
-        public void StructuredLog(string input)
-        {
-            _logger.LogInformation(
-                "Input text: {Input}",
-                input);
-        }
+    [HttpGet]
+    public void EnrichLog(string input, string inputType)
+    {
+        _logger.LogInformation("Input text: {Input} with {InputType}", input, inputType);
+    }
 
-        [HttpGet]
-        public void EnrichLog(string input, string inputType)
-        {
-            _logger.LogInformation("Input text: {Input} with {InputType}", input, inputType);
-        }
+    [HttpGet]
+    public void ScopeLog(string input, string additionalContext)
+    {
+        // Variable _ is discarded and this scope lasts until the end of the method.
+        // You can still use using with curly brackets as before if you need it to limit scope or readability preference.
+        // There a few utils that can simplify the code: https://github.com/jernejk/EfCoreSamples.Logging/blob/master/EfCoreSamples.Logging.Persistence/Utils/LoggingExtensions.cs
+        using var _ = _logger.BeginScope(new Dictionary<string, object> { { "AdditionalContext", additionalContext } });
 
-        [HttpGet]
-        public void ScopeLog(string input, string additionalContext)
-        {
-            using (_logger.BeginScope(new Dictionary<string, object> { { "AdditionalContext", additionalContext } }))
-            {
-                _logger.LogInformation("Input text with scope: {Input}", input);
+        _logger.LogInformation("Input text with scope: {Input}", input);
 
-                MoreWork();
-            }
-        }
+        MoreWork();
+    }
 
-        [HttpGet]
-        public async Task TimeLog()
-        {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            await Task.Delay(new Random().Next(2000));
-            stopwatch.Stop();
-            _logger.LogInformation("Long task: {LongTimeElapsedMs}ms", stopwatch.ElapsedMilliseconds);
-        }
+    [HttpGet]
+    public async Task TimeLog()
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        await Task.Delay(new Random().Next(2000));
+        stopwatch.Stop();
 
-        private void MoreWork()
-        {
-            _logger.LogInformation("More log context");
-        }
+        _logger.LogInformation("Long task: {LongTimeElapsedMs}ms", stopwatch.ElapsedMilliseconds);
+    }
+
+    private void MoreWork()
+    {
+        // This will have additional properties in the structured log if called inside a log context.
+        _logger.LogInformation("More logs");
     }
 }
