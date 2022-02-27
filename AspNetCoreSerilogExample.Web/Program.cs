25c6c1923b3aafab34c922fec092e8b1c3a8429a
Jernej Kavka (JK) <jk@ssw.com.au>
2022-02-27 15:10:40 +1000

    ✨ Upgraded Serilog project to .NET 6.

--- AspNetCoreSerilogExample.Web/Program.cs
+++ AspNetCoreSerilogExample.Web/Program.cs
@@ -1,78 +1,80 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+﻿// Logging based on https://jkdev.me/asp-net-core-serilog/ and https://github.com/datalust/dotnet6-serilog-example
+// NOTE: When upgrading from .NET 5 or earlier, add `<ImplicitUsings>enable</ImplicitUsings>` to **.csproj** file under `<PropertyGroup>`.
+// NOTE: While you can still use full Program.cs and Startup.cs, `.UseSerilog()` is marked as obsolete for them. It's safer to move to minimal APIs.
 using Serilog;
-using System;
 using System.Diagnostics;
 
-namespace AspNetCoreSerilogExample.Web
+try
 {
-    /// <summary>
-    /// Logging based on blog post https://jkdev.me/asp-net-core-serilog/
-    /// </summary>
-    public static class Program
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, loggerConfiguration) =>
     {
-        public static void Main(string[] args)
-        {
-            try
-            {
-                using IHost host = CreateHostBuilder(args).Build();
-                host.Run();
-            }
-            catch (Exception ex)
-            {
-                // Log.Logger will likely be internal type "Serilog.Core.Pipeline.SilentLogger".
-                if (Log.Logger == null || Log.Logger.GetType().Name == "SilentLogger")
-                {
-                    // Loading configuration or running the application failed.
-                    // This will create a logger that can be captured by Azure logger.
-                    // To enable Azure logger, in Azure Portal:
-                    // 1. Go to WebApp
-                    // 2. App Service logs
-                    // 3. Enable "Application Logging (Filesystem)", "Application Logging (Filesystem)" and "Detailed error messages"
-                    // 4. Set Retention Period (Days) to 10 or similar value
-                    // 5. Save settings
-                    // 6. Under Overview, restart web app
-                    // 7. Go to Log Stream and observe the logs
-                    Log.Logger = new LoggerConfiguration()
-                        .MinimumLevel.Debug()
-                        .WriteTo.Console()
-                        .CreateLogger();
-                }
-
-                Log.Fatal(ex, "Host terminated unexpectedly");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-            => Host.CreateDefaultBuilder(args)
-                   .ConfigureWebHostDefaults(webBuilder =>
-                   {
-                       webBuilder.UseStartup<Startup>()
-                        .CaptureStartupErrors(true)
-                        .ConfigureAppConfiguration(config =>
-                        {
-                            config
-                                // Used for local settings like connection strings.
-                                .AddJsonFile("appsettings.Local.json", optional: true);
-                        })
-                        .UseSerilog((hostingContext, loggerConfiguration) => {
-                            loggerConfiguration
-                                .ReadFrom.Configuration(hostingContext.Configuration)
-                                .Enrich.FromLogContext()
-                                .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
-                                .Enrich.WithProperty("Environment", hostingContext.HostingEnvironment);
+        loggerConfiguration
+            .ReadFrom.Configuration(ctx.Configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
+            .Enrich.WithProperty("Environment", ctx.HostingEnvironment);
 
 #if DEBUG
-                            // Used to filter out potentially bad data due debugging.
-                            // Very useful when doing Seq dashboards and want to remove logs under debugging session.
-                            loggerConfiguration.Enrich.WithProperty("DebuggerAttached", Debugger.IsAttached);
+        // Used to filter out potentially bad data due debugging.
+        // Very useful when doing Seq dashboards and want to remove logs under debugging session.
+        loggerConfiguration.Enrich.WithProperty("DebuggerAttached", Debugger.IsAttached);
 #endif
-                        });
-                   });
+    });
+
+    // Register services
+    builder.Services.AddControllers();
+    builder.Services.AddLogging();
+
+
+    WebApplication app = builder.Build();
+
+    // Rest of configuration.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
     }
+
+    // This will make the HTTP requests log as rich logs instead of plain text.
+    app.UseSerilogRequestLogging();
+
+    app.UseRouting();
+
+    // Absolute minimum setup, just return "Hello world!" to browser.
+    // You can use Controllers, SPA routing, SignalR, etc. routing.
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
+        endpoints.MapGet("", context => context.Response.WriteAsync("Hello World!\nUse /api/test/flatlog?input=Test, /api/test/StructuredLog?input=Test, etc. and observe console/Seq for logs."));
+    });
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    if (Log.Logger == null || Log.Logger.GetType().Name == "SilentLogger")
+    {
+        // Loading configuration or Serilog failed.
+        // This will create a logger that can be captured by Azure logger.
+        // To enable Azure logger, in Azure Portal:
+        // 1. Go to WebApp
+        // 2. App Service logs
+        // 3. Enable "Application Logging (Filesystem)", "Application Logging (Filesystem)" and "Detailed error messages"
+        // 4. Set Retention Period (Days) to 10 or similar value
+        // 5. Save settings
+        // 6. Under Overview, restart web app
+        // 7. Go to Log Stream and observe the logs
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+    }
+
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
 }
